9e188e9d5ed6d71510eb7ce6b93f8daa2703aaa5
cryrodriguez13 <cryrodriguez13@yahoo.com>
2022-12-14 01:06:11 +0000

    lab 13

---
+++ lab13/main.tf
@@ -0,0 +1,72 @@
+
+variable "credentials_file" { 
+  default = "/home/cry8499/cis-91-361922-286e86948aff.json" 
+}
+
+variable "project" {
+  default = "cis-91-361922"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  region  = var.region
+  zone    = var.zone 
+  project = var.project
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "cis91-network"
+}
+
+resource "google_compute_instance" "webservers" {
+  count        = 3
+  name         = "web${count.index}"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  labels = {
+    role: "web"
+  }
+}
+
+resource "google_compute_firewall" "default-firewall" {
+  name = "default-firewall"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports = ["22", "80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+output "external-ip" {
+  value = google_compute_instance.webservers[*].network_interface[0].access_config[0].nat_ip
+}
